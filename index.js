25defd386f71c4ef93da384e6318dc76b87b9517
Israel Dias <israeldiasoliveira97@gmail.com>
2025-09-06 11:04:37 -0300

    Iniciando servidor node.js no projeto de react

---
+++ index.js
@@ -0,0 +1,14 @@
+      
+      const container = document.querySelector('#container');
+       
+        const element = () => {
+            return(
+                <div>
+                    <h1>Helo World!</h1>;
+                    <p>Welcome to React!</p>
+                </div>
+                );
+            };
+
+        ReactDOM.createRoot(container).render(element());
+        
